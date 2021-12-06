ca6d079ffa79175c6cbba2b8e453685e123eb5ef
Yana <75263925+Rachel228@users.noreply.github.com>
2021-12-06 16:01:33 +0200

    Kyrsova

--- Online Store/server/routes/productRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const productController = require('../controllers/productController')
-
-router.post('/', productController.create)
-router.get('/', productController.getAll)
-router.get('/:id', productController.getOne)
-
-module.exports = router
